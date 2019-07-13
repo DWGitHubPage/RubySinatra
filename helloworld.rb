0088499013597906cdcd5b6cb75d9d8c2f19c891
D. W <owl.robot@aol.com>
2019-07-12 22:56:27 -0400

    Update helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -3,8 +3,10 @@ puts "Hello, World!"
 puts "Ruby Go Home!"
 
 
-# Similar to Python.
-x = 2 + 4
-print(x)
+# Similar to Python. Can't have two print statements and a print and puts will be on same line
+# if print is before a puts statement.
 
+x = 2 + 4
+puts(x * x)
+puts (x)
 print(x * x)
