2e34799e4c5c2a02155e26c1bc05072e1f9cad45
D. W <owl.robot@aol.com>
2019-07-13 14:29:25 -0400

    Update rubysnippets.rb

--- rubysnippets.rb
+++ rubysnippets.rb
@@ -10,3 +10,30 @@ x = 2 + 4
 puts(x * x)
 puts (x)
 print(x * x)
+
+
+# Output multiple lines with one puts statement. 
+puts "This", "is", "a", "test"
+
+
+# String Concatenation.
+
+puts "Hello " + "World"
+
+color = 'yellow'
+
+puts "My favorite color is " + color + '.'
+
+output = "my favorite color is " + color + '.'
+puts output
+
+
+# Arrays
+
+numbers = [ 1, 2, 3, 4, 5, 6, 7,]
+puts numbers.size
+puts numbers [0]
+puts numbers [2]
+puts numbers [0], [1], [2]
+
+
